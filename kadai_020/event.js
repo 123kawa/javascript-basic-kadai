c3610d095d6eee2e59f17394aa3e488568c4b6e1
123kawa <kawai_tetsu@koushi-intec.co.jp>
2025-06-03 14:47:34 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-const cbtn = document.getElementById('btn');
-
-cbtn.addEventListener('click', () => {
-
-  let textBox = document.getElementById('text');
-
-  textBox.textContent = "ボタンをクリックしました";
-});
