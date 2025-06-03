dd305b78504a27f5a179fd30b547bf7f93e02b50
123kawa <kawai_tetsu@koushi-intec.co.jp>
2025-06-03 14:21:38 +0900

    js_kadai20

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const cbtn = document.getElementById('btn');
+
+cbtn.addEventListener('click', () => {
+
+  let textBox = document.getElementById('text');
+
+  textBox.textContent = "ボタンをクリックしました";
+});
